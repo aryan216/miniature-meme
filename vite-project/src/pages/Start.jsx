4a012cb8c754c7618a7d8b3f352094d6169f9d78
aryan216 <aniketani603@gmail.com>
2024-12-19 17:30:25 +0530

    start page ui initialized

--- vite-project/src/pages/Start.jsx
+++ vite-project/src/pages/Start.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import { IoTriangleSharp } from "react-icons/io5";
+import { FaCircle } from "react-icons/fa";
 const Start = () => {
   return (
-    <div>start</div>
+    <div>
+      <div className='flex'>
+        <div className='h-screen flex flex-col justify-center items-center w-1/3 bg-slate-100'>
+
+             <form className='relative flex gap-4 flex-col'>
+             <IoTriangleSharp className='absolute top-6 left-2' />
+             <FaCircle className='absolute top-20 left-2'/>
+              <div className='absolute top-8 left-3.5 h-12 w-0.5 bg-neutral-900'></div>
+              <input className='h-12 p-4 pl-8 w-72 rounded-sm '  type="text" placeholder='Enter your location'/>
+              <input className='h-12 p-4 pl-8 w-72 rounded-sm '  type="text" placeholder='Enter your Destination'/>
+             </form>
+        </div>
+        <div className='h-screen w-2/3 bg-slate-600'>
+          destination
+        </div>
+      </div>
+    </div>
   )
 }
 
